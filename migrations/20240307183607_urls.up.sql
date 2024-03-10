5e91d612d615a302b020555990979b6537bab5fe
Mario Maita <thetechpomelo@gmail.com>
2024-03-10 16:32:50 +0100

    add: diff users can short the same id

--- migrations/20240307183607_urls.up.sql
+++ migrations/20240307183607_urls.up.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE urls (
   id CHAR(8) PRIMARY KEY,
-  long_url VARCHAR(2048) NOT NULL UNIQUE,
+  long_url VARCHAR(2048) NOT NULL,
   user_id CHAR(8) NOT NULL,
   created_at TIMESTAMP NOT NULL
 );
